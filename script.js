3a766623b4af737ae76801608368abc6ccbbee6b
Imran <imranradziii@gmail.com>
2022-09-15 09:24:28 +0100

    some small style changes

--- script.js
+++ script.js
@@ -3,14 +3,11 @@ const observer = new IntersectionObserver((items) => {
     console.log(item);
     if (item.isIntersecting) {
       item.target.classList.add('show');
-    } else {
-      item.target.classList.remove('show');
-    }
+    } 
   });
 });
 
 const sections = document.querySelectorAll('.animation');
 
-sections.forEach((sec) => sec.classList.add('hidden'));
 sections.forEach((sec) => observer.observe(sec));
 
